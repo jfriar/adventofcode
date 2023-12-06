d6e474c78bf584f2b2049989a2388f96686411c3
John Friar <admjfriar@NOVL-JFRIAR2.2kgames.t2.corp>
2023-12-05 21:36:48 -0800

    day6 solutions

---
+++ 2023/day6_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import collections
+import re
+
+input_file = 'day6_input.txt'
+
+
+def parse_input(data):
+    race_times = []
+    max_dist = []
+    for line in data:
+        if line.startswith('Time:'):
+            race_times = [int(d) for d in re.findall(r"\d+", line)]
+        elif line.startswith('Distance:'):
+            max_dist = [int(d) for d in re.findall(r"\d+", line)]
+    return race_times, max_dist
+
+with open(input_file) as f:
+    data = [x.strip() for x in f.readlines()]
+
+race_times, max_dist = parse_input(data)
+
+def margin_of_error(race_time, distance):
+    set_speed_for_win = []
+    for s in range(race_time):
+        r = race_time - s
+        _d = s * r
+        if _d > distance:
+            set_speed_for_win.append(s)
+    # print(set_speed_for_win)
+    return len(set_speed_for_win)
+
+solve = []
+for i, race_time in enumerate(race_times):
+    moe = margin_of_error(race_time, max_dist[i])
+    solve.append(moe)
+
+result = 1
+for i in solve:
+    result *= i
+print(result)
---
+++ 2023/day6_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import collections
+import re
+
+input_file = 'day6_input.txt'
+
+
+def parse_input(data):
+    for line in data:
+        if line.startswith('Time:'):
+            race_time = int(''.join([d for d in re.findall(r"\d+", line)]))
+        elif line.startswith('Distance:'):
+            max_dist = int(''.join([d for d in re.findall(r"\d+", line)]))
+    return race_time, max_dist
+
+
+def margin_of_error(race_time, distance):
+    set_speed_for_win = []
+    for s in range(race_time):
+        r = race_time - s
+        _d = s * r
+        if _d > distance:
+            set_speed_for_win.append(s)
+    # print(set_speed_for_win)
+    return len(set_speed_for_win)
+
+
+with open(input_file) as f:
+    data = [x.strip() for x in f.readlines()]
+
+print(parse_input(data))
+race_time, max_dist = parse_input(data)
+moe = margin_of_error(race_time, max_dist)
+print(moe)
