40fedb4f5e577884b65fcea9764e615db77146e4
John Friar <admjfriar@NOVL-JFRIAR3.2kgames.t2.corp>
2024-12-06 00:51:05 -0800

    adding day02 part 1

---
+++ 2024/day02_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+input_file = 'day02_input.txt'
+# input_file = 'day02_sample.txt'
+
+
+def check_safe(report):
+    direction = 'increasing' if report[0] < report[1] else 'decreasing'
+    is_safe = True
+    for i, level in enumerate(report):
+        if i+1 == len(report):
+            # we have reached the end
+            break
+        next_level = report[i+1]
+        difference = abs(next_level - level)
+        if difference > 3 or difference < 1:
+            is_safe = False
+            break
+        if direction == 'increasing':
+            if level >= next_level:
+                is_safe = False
+                break
+        else:
+            if level <= next_level:
+                is_safe = False
+                break
+
+    return is_safe
+
+
+with open(input_file) as f:
+    safe_count = 0
+    for line in f.readlines():
+        report = [int(level) for level in line.split()]
+        is_safe = check_safe(report)
+        if is_safe:
+            safe_count += 1
+            print(line.rstrip(), " safe")
+        else:
+            print(line.rstrip(), " not safe")
+    print('\nsafe count:', safe_count)
+
+
